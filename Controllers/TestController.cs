249b80bc0815d48495ef466d8a9de217fb8a8de0
Anchal Chaudhary <anchal.ldn5@gmail.com>
2017-06-15 00:31:42 +0530

    ViewData and ViewBag

---
+++ Controllers/TestController.cs
@@ -0,0 +1,41 @@
+﻿using MVCTutorial.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCTutorial.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            //ViewBag.Name = "Anchal";
+            //List<string> list = new List<string>();
+            //list.Add("Anchal");
+   
+            //ViewBag.NameList = list;
+
+            List<Names> namesList = new List<Names>();
+            Names name = new Names();
+
+            namesList.Add(new Names { ID = 1, Name = "Anchal", Branch = "IT" });
+            namesList.Add(new Names { ID = 2, Name = "Kanika", Branch = "EC" });
+            namesList.Add(new Names { ID = 3, Name = "Simran", Branch = "IT" });
+            namesList.Add(new Names { ID = 4, Name = "Ayush", Branch = "IT" });
+            namesList.Add(new Names { ID = 5, Name = "Nakshtra", Branch = "IT" });
+
+            ViewBag.NamesList = namesList;
+            ViewData["NamesList"] = namesList;
+            
+            return View(namesList);
+        }
+
+        public ActionResult  SecondPage()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Names.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCTutorial.Models
+{
+    public class Names
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Branch { get; set; }
+
+    }
+}
