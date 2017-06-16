7d4cc24a965e3fce65ff683b7a861855280379e9
Anchal Chaudhary <anchal.ldn5@gmail.com>
2017-06-17 00:47:28 +0530

    DB integration

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -12,31 +12,31 @@ namespace MVCTutorial.Controllers
         // GET: Test
         public ActionResult Index()
         {
-            //ViewBag.Name = "Anchal";
-            //List<string> list = new List<string>();
-            //list.Add("Anchal");
-   
-            //ViewBag.NameList = list;
+            //List<Names> namesList = new List<Names>();
+            //Names name = new Names();
 
-            List<Names> namesList = new List<Names>();
-            Names name = new Names();
+            //namesList.Add(new Names { ID = 1, Name = "Anchal", Branch = "IT" });
+                        
+            //ViewBag.NamesList = namesList;
+            //ViewData["NamesList"] = namesList;
 
-            namesList.Add(new Names { ID = 1, Name = "Anchal", Branch = "IT" });
-            namesList.Add(new Names { ID = 2, Name = "Kanika", Branch = "EC" });
-            namesList.Add(new Names { ID = 3, Name = "Simran", Branch = "IT" });
-            namesList.Add(new Names { ID = 4, Name = "Ayush", Branch = "IT" });
-            namesList.Add(new Names { ID = 5, Name = "Nakshtra", Branch = "IT" });
+            //ViewBag.NameVB = "Anchal";
+            //ViewData["NameVD"] = "Salvee";
+            //TempData["NameTD"] = "Ayushi";
 
-            ViewBag.NamesList = namesList;
-            ViewData["NamesList"] = namesList;
+            //TempData.Keep();
 
-            ViewBag.NameVB = "Anchal";
-            ViewData["NameVD"] = "Salvee";
-            TempData["NameTD"] = "Ayushi";
+            //return View(namesList);
 
-            TempData.Keep();
-            
-            return View(namesList);
+            MVCTutorialEntities db = new MVCTutorialEntities();
+            tblName name = db.tblNames.SingleOrDefault(x => x.ID == 1);
+
+            ViewNames objVM = new ViewNames();
+            objVM.ID = name.ID;
+            objVM.Name = name.Name;
+            objVM.Branch = name.Branch;
+
+            return View(objVM);
         }
 
         public ActionResult SecondPage()
---
+++ Models/ViewNames.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCTutorial.Models
+{
+    public class ViewNames
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Branch { get; set; }
+    }
+}
